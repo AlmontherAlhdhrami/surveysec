996407869a10541d727ecd936e181373bee7c7af
AlmontherAlhdhrami <116969342+AlmontherAlhdhrami@users.noreply.github.com>
2025-02-19 16:37:48 +0400

    up

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [
-        require('@tailwindcss/postcss'), // Use the PostCSS plugin here as well
-      ],
+  base: "./", // Ensures assets load correctly in production
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  },
+  preview: {
+    allowedHosts: ["https://surveysec.onrender.com"],
   },
 });
