22ee5a74855e4a28f709eee415e0aa54591f6a39
AlmontherAlhdhrami <116969342+AlmontherAlhdhrami@users.noreply.github.com>
2025-02-22 00:08:02 +0400

    up

--- src/assets/createClient.js
+++ src/assets/createClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Load environment variables
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = 'https://jnkvjxtheefxahaxssey.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impua3ZqeHRoZWVmeGFoYXhzc2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMDUwMDgsImV4cCI6MjA1Mjg4MTAwOH0.VJwWViDZYOWGDpL16Hk0NM_90UsJzV3AMDdTqaQCcSk';
 
 // Initialize Supabase client
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
